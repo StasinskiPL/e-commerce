4a6f27b689169f50ae4edd53f5fa27164ba8d73a
GreenDeer2001 <01DawidS@gmail.com>
2021-02-17 08:59:56 +0100

    adding form lib 📝

--- src/components/Admin/AddProductForm.tsx
+++ src/components/Admin/AddProductForm.tsx
@@ -1,102 +1,89 @@
-import { useState, useRef, FormEvent, useEffect } from "react";
+import { useRef } from "react";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
 import { postProducts } from "../../store/productsSlice";
 import { useDispatch } from "react-redux";
 import { Product } from "../../types";
 import categories from "../../assets/data/categories";
 import { auth } from "../../firebase";
 
+interface FormTypes {
+  name: string;
+  price: number;
+  description: string;
+  category: string;
+  image: string;
+  additionalImg1: string;
+  additionalImg2: string;
+  additionalImg3: string;
+}
+
+let schema = yup.object().shape({
+  name: yup.string().required(),
+  price: yup.number().required().positive(),
+  description: yup.string().required(),
+  category: yup.string(),
+  image: yup.string().required().url(),
+  additionalImg1: yup.string().url(),
+  additionalImg2: yup.string().url(),
+  additionalImg3: yup.string().url(),
+});
+
 const AddProductForm = () => {
   const form = useRef<HTMLFormElement>(null!);
-  const name = useRef<HTMLInputElement>(null!);
-  const description = useRef<HTMLTextAreaElement>(null!);
-  const category = useRef<HTMLSelectElement>(null!);
-  const image = useRef<HTMLInputElement>(null!);
-  const additionImages1 = useRef<HTMLInputElement>(null!);
-  const additionImages2 = useRef<HTMLInputElement>(null!);
-  const additionImages3 = useRef<HTMLInputElement>(null!);
-  const price = useRef<HTMLInputElement>(null!);
-
-  const [nameError, setNameError] = useState<boolean>(false);
-  const [descriptionError, setDescriptionError] = useState<boolean>(false);
-  const [imageError, setImageError] = useState<boolean>(false);
-  const [priceError, setPriceError] = useState<boolean>(false);
-  const [isAdmin, setIsAdmin] = useState<boolean>(false);
-
   const dispatch = useDispatch();
 
-  useEffect(() => {
+  const { register, handleSubmit, errors } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const addProductHandler = (data: FormTypes) => {
+    const additionalImages: string[] = [
+      data.additionalImg1,
+      data.additionalImg2,
+      data.additionalImg3,
+    ].filter((link) => link);
+
+    const prod: Product = {
+      name: data.name,
+      description: data.description,
+      category: data.category,
+      price: data.price,
+      mainImage: data.image,
+      additionalImages: additionalImages,
+    };
     if (auth.currentUser && auth.currentUser.email === "dawid1@gmail.com") {
-      setIsAdmin(true);
+      dispatch(postProducts(prod));
     }
-  }, []);
-
-  const addProductHandler = (e: FormEvent) => {
-    e.preventDefault();
-    if (
-      name.current.value.trim() &&
-      description.current.value.trim() &&
-      category.current.value &&
-      image.current.value.trim() &&
-      price.current.value.toString().trim()
-    ) {
-      const additionalImages: string[] = [];
-      additionImages1.current.value &&
-        additionalImages.push(additionImages1.current.value);
-      additionImages2.current.value &&
-        additionalImages.push(additionImages2.current.value);
-      additionImages3.current.value &&
-        additionalImages.push(additionImages3.current.value);
-
-      const prod: Product = {
-        name: name.current.value,
-        description: description.current.value,
-        category: category.current.value,
-        mainImage: image.current.value,
-        price: +price.current.value,
-        additionalImages: additionalImages,
-      };
-      if (auth.currentUser && auth.currentUser.email === "dawid1@gmail.com") {
-        dispatch(postProducts(prod));
-      }
-      form.current.reset();
-      setNameError(false);
-      setDescriptionError(false);
-      setPriceError(false);
-      setImageError(false);
-    } else {
-      name.current.value ? setNameError(false) : setNameError(true);
-      description.current.value
-        ? setDescriptionError(false)
-        : setDescriptionError(true);
-      price.current.value ? setPriceError(false) : setPriceError(true);
-      image.current.value ? setImageError(false) : setImageError(true);
-    }
+    form.current.reset();
   };
+
   return (
     <form
       ref={form}
       className="admin__addForm"
-      onSubmit={(e) => addProductHandler(e)}
+      onSubmit={handleSubmit(addProductHandler)}
     >
-      <label className={`${nameError && "admin-error"}`} htmlFor="name">
+      <label className={`${errors.name && "admin-error"}`} htmlFor="name">
         Name:
       </label>
-      <input ref={name} name="name" type="text" />
+      <input ref={register} name="name" type="text" />
 
-      <label className={`${priceError && "admin-error"}`} htmlFor="price">
+      <label className={`${errors.price && "admin-error"}`} htmlFor="price">
         Price:
       </label>
-      <input ref={price} name="price" type="number" step="0.01" />
+      <input ref={register} name="price" type="text" />
 
-      <label htmlFor="desc">Description:</label>
+      <label htmlFor="description">Description:</label>
       <textarea
-        className={`${descriptionError && "admin-error-textarea"}`}
-        ref={description}
-        name="Description"
+        className={`${errors.description && "admin-error-textarea"}`}
+        ref={register}
+        name="description"
       />
 
       <label htmlFor="category">Category:</label>
-      <select ref={category} name="category">
+      <select ref={register} name="category">
         {categories.slice(1).map((catgr, index) => (
           <option key={index} value={catgr}>
             {catgr}
@@ -104,20 +91,20 @@ const AddProductForm = () => {
         ))}
       </select>
 
-      <label className={`${imageError && "admin-error"}`} htmlFor="Image">
+      <label className={`${errors.image && "admin-error"}`} htmlFor="image">
         Image(url):
       </label>
-      <input ref={image} name="Image" type="text" />
+      <input ref={register} name="image" type="text" />
 
-      <label htmlFor="additionalImg">Additional Images:</label>
-      <input ref={additionImages1} name="additionalImg" type="text" />
-      <input ref={additionImages2} name="additionalImg" type="text" />
-      <input ref={additionImages3} name="additionalImg" type="text" />
+      <label htmlFor="additionalImg1">Additional Images:</label>
+      <input ref={register} name="additionalImg1" type="text" />
+      <input ref={register} name="additionalImg2" type="text" />
+      <input ref={register} name="additionalImg3" type="text" />
 
       <button
         className="admin__addForm-btn"
         title="You aren't Admin"
-        disabled={!isAdmin}
+        disabled
         type="submit"
       >
         Submit
